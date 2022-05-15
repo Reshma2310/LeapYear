62e24e7503594a0104438690a134e6be64fdf35a
Reshma2310 <dreshma253@gmail.com>
2022-05-15 14:06:47 +0500

    [Reshma] - Lear Year Program

---
+++ LeapYear/LeapYear/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace LeapYear
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int year;
+            Console.WriteLine("Enter an Year");
+            year = Convert.ToInt32(Console.ReadLine());
+            if ((year%4 == 0) && (year%100 != 0))
+            {
+                Console.WriteLine("Leap Year");
+            }
+            else
+            {
+                Console.WriteLine("Not a Leap Year");
+            }
+        }
+    }
+}
